77317e1bf9a576c24fbc1c9013f588ac85cc9813
Aleksandra Haniok <a.haniok@gmail.com>
2022-02-25 13:10:20 +0000

    Amend function to receive repository data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -18,7 +18,6 @@ function userInformationHTML(user) {
 }
 
 
-
 function fetchGitHubInformation(event) {
     var username = $('#gh-username').val();
     if (!username) {
@@ -34,10 +33,14 @@ function fetchGitHubInformation(event) {
         //  promise
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
+        // lists repositories for this individual user
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $('#gh-user-data').html(userInformationHTML(userData));
+            $('#gh-repo-data').html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $('#gh-user-data').html(`<h2>No information found for user ${username}</h2>`)
