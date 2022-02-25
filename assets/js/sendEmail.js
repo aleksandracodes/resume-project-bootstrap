f18758c558e8eb1fe9f42732be4558315a89b361
Aleksandra Haniok <a.haniok@gmail.com>
2022-02-25 16:46:02 +0000

    Implement EmailJS integration on the contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,26 @@
-console.log('hi')
+function sendMail(contactForm) {
+    emailjs.send('rosie-resume', 'rosie-resume', {
+        // parameter names set in the html file
+        'from_name': contactForm.name.value,
+        'from_email': contactForm.emailaddress.value,
+        'project_request': contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log('SUCCESS', response);
+            formSubmittedMessage();
+        },
+        function(error) {
+            console.log('FAILED', error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
+
+function formSubmittedMessage() {
+    let message = `<div class="text-center">
+                        <h2>Thanks, your message has been sent</h2>
+                   </div>`;
+    document.getElementById("form").innerHTML = message;
+}
